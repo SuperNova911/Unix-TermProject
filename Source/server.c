c10fc034a311c1c4ff6ea69bbb14c82e82269ac0
Dopa99 <tychus77@naver.com>
2018-11-20 00:45:16 +0900

    initiateServer, async, acceptClient, receiveData 구현

--- Source/server.c
+++ Source/server.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <ncurses.h>
 #include <stdbool.h>
@@ -14,6 +15,8 @@
 #include <sys/types.h>
 
 #define SERVER_PORT 10743
+#define SERVER_ADDRESS "192.168.98.128"
+#define MAX_CLIENT 64
 
 enum Command
 {
@@ -28,6 +31,9 @@ struct DataPack
 
 // 서버
 bool initiateServer();
+void async();
+void acceptClient(int socket);
+void receiveData(int socket);
 
 // 통신
 bool receiveDataPack(int sender, struct DataPack *dataPack);
@@ -39,6 +45,7 @@ void drawBorder(WINDOW *window, char *windowName);
 void onClose();
 
 int ServerSocket;
+time_t ServerUpTime;
 
 int Fdmax;
 fd_set Master, Reader;
@@ -53,15 +60,154 @@ int main()
     initiateInterface();
 
     initiateServer();
-
-    sleep(10);
+    async();
     
     return 0;
 }
 
 bool initiateServer()
 {
-    return false;
+    wprintw(MessageWindow, "Initiate server\n");
+    wrefresh(MessageWindow);
+
+    ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (ServerSocket == -1)
+    {
+        wprintw(MessageWindow, "socket: %s\n", strerror(errno));
+        wrefresh(MessageWindow);
+        return false;
+    }
+
+    int socketOption = 1;
+    setsockopt(ServerSocket, SOL_SOCKET, SO_REUSEADDR, &socketOption, sizeof(socketOption));
+
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(struct sockaddr_in));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    // serverAddr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+
+    if (bind(ServerSocket, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr_in)) == -1)
+    {
+        wprintw(MessageWindow, "bind: %s\n", strerror(errno));
+        wrefresh(MessageWindow);
+        return false;
+    }
+
+    if (listen(ServerSocket, MAX_CLIENT) == -1)
+    {
+        wprintw(MessageWindow, "listen: %s\n", strerror(errno));
+        wrefresh(MessageWindow);
+        return false;
+    }
+
+    time(&ServerUpTime);
+
+    return true;
+}
+
+void async()
+{
+    FD_ZERO(&Master);
+    FD_ZERO(&Reader);
+    FD_SET(ServerSocket, &Master);
+    Fdmax = ServerSocket;
+
+    while (true)
+    {
+        wprintw(MessageWindow, "Wait for data...\n");
+        wrefresh(MessageWindow);
+
+        Reader = Master;
+        if (select(Fdmax + 1, &Reader, NULL, NULL, NULL) == -1)
+        {
+            wprintw(MessageWindow, "select: %s\n", strerror(errno));
+            wrefresh(MessageWindow);
+            return;
+        }
+
+        for (int fd = 0; fd <= Fdmax; fd++)
+        {
+            if (FD_ISSET(fd, &Reader) == 0)
+                continue;
+
+            if (fd == ServerSocket)
+            {
+                acceptClient(fd);
+            }
+            else if (fd == 0)   // stdin
+            {
+
+            }
+            else
+            {
+                receiveData(fd);
+            }
+        }
+    }
+}
+
+void acceptClient(int socket)
+{
+    struct sockaddr_in clientAddr;
+    socklen_t addrlen = sizeof(struct sockaddr_in);
+
+    socket = accept(ServerSocket, (struct sockaddr *)&clientAddr, &addrlen);
+    if (socket == -1)
+    {
+        wprintw(MessageWindow, "accept: %s\n", strerror(errno));
+        wrefresh(MessageWindow);
+    }
+    else
+    {
+        FD_SET(socket, &Master);
+        if (socket > Fdmax)
+        {
+            Fdmax = socket;
+        }
+        wprintw(MessageWindow, "Accept new client, socket: '%d'\n", socket);
+        wrefresh(MessageWindow);
+    }
+}
+
+void receiveData(int socket)
+{
+    int readBytes;
+    struct DataPack *receivedDataPack;
+    // memset(receivedDataPack, 0, sizeof(struct DataPack));
+    char buffer[sizeof(struct DataPack)] = { 0, };
+
+    readBytes = recv(socket, buffer, sizeof(buffer), 0);
+    switch (readBytes)
+    {
+        case -1:
+            close(socket);
+            FD_CLR(socket, &Master);
+            wprintw(MessageWindow, "recv: %s, socket: %d\n", strerror(errno), socket);
+            wrefresh(MessageWindow);
+            break;
+
+        case 0:
+            close(socket);
+            FD_CLR(socket, &Master);
+            wprintw(MessageWindow, "Client disconnected, socket: '%d'\n", socket);
+            wrefresh(MessageWindow);
+            break;
+
+        default:
+            if (readBytes == sizeof(struct DataPack))
+            {
+                receivedDataPack = (struct DataPack *)buffer;
+                receiveDataPack(socket, receivedDataPack);
+            }
+            else
+            {
+                wprintw(MessageWindow, "Invalid data received, socket: '%d', readBytes: '%d'\n", socket, readBytes);
+                wrefresh(MessageWindow);
+            }
+            break;
+    }
 }
 
 bool receiveDataPack(int sender, struct DataPack *dataPack)
