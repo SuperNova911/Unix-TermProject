299d9fec44afa5c342772fb9fac53bea10c33ca4
Dopa99 <tychus77@naver.com>
2018-11-26 15:49:00 +0900

    server파일도 세분화된 헤더파일들로 분리 중

--- Source/server.c
+++ Source/server.c
@@ -1,7 +1,9 @@
+#include "dataPack.h"
+#include "interface.h"
+
 #include <errno.h>
 #include <fcntl.h>
 #include <locale.h>
-#include <ncurses.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,47 +20,9 @@
 #define SERVER_PORT 10743
 #define SERVER_ADDRESS "192.168.98.128"
 #define MAX_CLIENT 64
+#define MAX_LECTURE 16
 
-enum Command
-{
-    NONE,
-
-    USER_LOGIN_REQUEST, USER_LOGOUT_REQUEST,
-    LECTURE_LIST_REQUEST, LECTURE_CREATE_REQUEST, LECTURE_REMOVE_REQUEST,
-    LECTURE_ENTER_REQUEST, LECTURE_LEAVE_REQUEST, LECTURE_REGISTER_REQUEST, LECTURE_DEREGISTER_REQUEST,
-    ATTENDANCE_START_REQUEST, ATTNEDANCE_STOP_REQUEST, ATTENDANCE_RESULT_REQUEST, ATTENDANCE_CHECK_REQUEST,
-    CHAT_ENTER_REQUEST, CHAT_LEAVE_REQUEST, CHAT_USER_LIST_REQUEST, CHAT_SEND_MESSAGE_REQUEST,
-    
-    LOGIN_RESPONSE, LOGOUT_RESPONSE,
-    LECTURE_LIST_RESPONSE, LECTURE_CREATE_RESPONSE, LECTURE_REMOVE_RESPONSE,
-    LECTURE_ENTER_RESPONSE, LECTURE_LEAVE_RESPONSE, LECTURE_REGISTER_RESPONSE, LECTURE_DEREGISTER_RESPONSE,
-    ATTENDANCE_START_RESPONSE, ATTNEDANCE_STOP_RESPONSE, ATTENDANCE_RESULT_RESPONSE, ATTENDANCE_CHECK_RESPONSE,
-    CHAT_ENTER_RESPONSE, CHAT_LEAVE_RESPONSE, CHAT_USER_LIST_RESPONSE, CHAT_SEND_MESSAGE_RESPONSE,
-};
-
-enum UserRole
-{
-    None, Admin, Student, Professor
-};
-
-typedef struct UserInfo_t
-{
-    enum UserRole role;
-    char userName[16];
-    char studentID[16];
-} UserInfo;
-
-typedef struct DataPack_t
-{
-    enum Command command;
-    bool result;
-    char data1[128];
-    char data2[128];
-    char data3[128];
-    char data4[128];
-    char message[508 - sizeof(UserInfo)];
-    UserInfo userInfo;
-} DataPack;
+void initializeGlobalVariable();
 
 // 서버
 bool initiateServer();
@@ -70,12 +34,19 @@ bool sendDataPack(int receiver, DataPack *dataPack);
 // 통신
 bool decomposeDataPack(int sender, DataPack *dataPack);
 
-// 인터페이스
-void initiateInterface();
-void drawDefaultLayout();
-void drawBorder(WINDOW *window, char *windowName);
-void printMessage(WINDOW *window, const char *format, ...);
-void onClose();
+void userLogin(int receiver, char *studentID, char *password);
+void userLogout(int receiver, UserInfo *userInfo);
+
+void lectureList(int receiver);
+void lectureCreate(int receiver, char *lectureName);
+void lectureRemove(int receiver, char *lectureName);
+void lectureEnter(int receiver, char *lectureName);
+void lectureLeave(int receiver, char *lectureName);
+void lectureRegister(int receiver, char *lectureName);
+void lectureDeregister(int receiver, char *lectureName);
+
+User userList[MAX_CLIENT];
+Lecture lectureList[MAX_LECTURE];
 
 int ServerSocket;
 time_t ServerUpTime;
@@ -83,20 +54,33 @@ time_t ServerUpTime;
 int Fdmax;
 fd_set Master, Reader;
 
-WINDOW *MessageWindow, *MessageWindowBorder;
-WINDOW *CommandWindow, *CommandWindowBorder;
-WINDOW *InputWindow, *InputWindowBorder;
+extern WINDOW *MessageWindow, *MessageWindowBorder;
+extern WINDOW *CommandWindow, *CommandWindowBorder;
+extern WINDOW *InputWindow, *InputWindowBorder;
 
 int main()
 {
+    initializeGlobalVariable();
+    
     atexit(onClose);
     initiateInterface();
+
     drawDefaultLayout();
 
     initiateServer();
+
     async();
     
     return 0;
+}
+
+// 전역 변수 초기화
+void initializeGlobalVariable()
+{
+    for (int index = 0; index < MAX_CLIENT; index++)
+        memset(&userList[index], 0, sizeof(User));
+    for (int index = 0; index < MAX_LECTURE; index++)
+        memset(&lectureList[index], 0, sizeof(Lecture));
 }
 
 // TCP/IP 소켓 서버 시작
@@ -297,102 +281,99 @@ bool decomposeDataPack(int sender, DataPack *dataPack)
     }
 }
 
+void userLogin(int receiver, char *studentID, char *password)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    // if (loginUser(studentID, password))
+    // {
+    //     User user = loadUserByID(studentID);
+
+    //     dataPack.result = true;
+    //     dataPack.userInfo.role = user.role;
+    //     strncpy(dataPack.userInfo.userName, user.userName, sizeof(dataPack.userInfo.userName));
+    //     strncpy(dataPack.userInfo.studentID, user.studentID, sizeof(dataPack.userInfo.studentID));
+    //     return true;
+    // }
+    // else
+    // {
+    //     dataPack.result = false;
+    //     strncpy(dataPack.message, "학번 또는 비밀번호가 틀립니다.");
+    //     return false;
+    // }
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void userLogout(int receiver, UserInfo *userInfo)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void lectureList(int receiver)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void lectureCreate(int receiver, char *lectureName)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void lectureRemove(int receiver, char *lectureName)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void lectureEnter(int receiver, char *lectureName)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void lectureLeave(int receiver, char *lectureName)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void lectureRegister(int receiver, char *lectureName)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
+void lectureDeregister(int receiver, char *lectureName)
+{
+    DataPack dataPack;
+    resetDataPack(&dataPack);
+
+    sendDataPack(receiver, &dataPack);
+}
+
 // 클라이언트에게 DataPack을 전송
 // [매개변수] receiver: 전송 대상 클라이언트 소켓, dataPack: 전송 할 DataPack
 // [반환] true: 전송 성공, false: 전송 실패
 bool sendDataPack(int receiver, DataPack *dataPack)
 {
     return false;
-}
-
-// ncurses라이브러리를 이용한 사용자 인터페이스 초기화
-void initiateInterface()
-{
-    setlocale(LC_CTYPE, "ko_KR.utf-8");
-    initscr();
-    noecho();
-    curs_set(FALSE);
-}
-
-// 기본 레이아웃으로 윈도우 그리기
-void drawDefaultLayout()
-{
-    int parentX, parentY;
-    getmaxyx(stdscr, parentY, parentX);
-
-    int commandWindowBorderWidth = 20;
-    int userInputWindowBorderHeight = 4;
-
-    MessageWindow = newwin(parentY - userInputWindowBorderHeight - 2, parentX - commandWindowBorderWidth - 2, 1, 1);
-    MessageWindowBorder = newwin(parentY - userInputWindowBorderHeight, parentX - commandWindowBorderWidth, 0, 0);
-
-    CommandWindow = newwin(parentY - userInputWindowBorderHeight - 2, commandWindowBorderWidth - 2, 1, parentX - commandWindowBorderWidth + 1);
-    CommandWindowBorder = newwin(parentY - userInputWindowBorderHeight, commandWindowBorderWidth, 0, parentX - commandWindowBorderWidth);
-
-    InputWindow = newwin(userInputWindowBorderHeight - 2, parentX - 2, parentY - userInputWindowBorderHeight + 1, 1);
-    InputWindowBorder = newwin(userInputWindowBorderHeight, parentX, parentY - userInputWindowBorderHeight, 0);
-
-    scrollok(MessageWindow, TRUE);
-    scrollok(CommandWindow, TRUE);
-
-    drawBorder(MessageWindowBorder, "MESSAGE");
-    drawBorder(CommandWindowBorder, "COMMAND");
-    drawBorder(InputWindowBorder, "USER INPUT");
-    
-    wrefresh(MessageWindow);
-    wrefresh(CommandWindow);
-    wrefresh(InputWindow);
-}
-
-// 윈도우 테두리 그리기
-// [매개변수] window: 테두리를 그릴 윈도우, windowName: 상단에 보여줄 윈도우 이름
-void drawBorder(WINDOW *window, char *windowName)
-{
-    int x, y;
-    getmaxyx(window, y, x);
-
-    // 테두리 그리기
-    mvwprintw(window, 0, 0, "+"); 
-    mvwprintw(window, y - 1, 0, "+"); 
-    mvwprintw(window, 0, x - 1, "+"); 
-    mvwprintw(window, y - 1, x - 1, "+"); 
-    for (int i = 1; i < (y - 1); i++)
-    {
-        mvwprintw(window, i, 0, "|"); 
-        mvwprintw(window, i, x - 1, "|"); 
-    }
-    for (int i = 1; i < (x - 1); i++)
-    {
-        mvwprintw(window, 0, i, "-"); 
-        mvwprintw(window, y - 1, i, "-"); 
-    }
-
-    // 윈도우 이름 출력
-    mvwprintw(window, 0, 4, windowName); 
-
-    wrefresh(window);
-}
-
-// 매개변수로 전달한 윈도우에 문자열 출력
-// [매개변수] window: 문자열을 출력할 윈도우, format: 문자열 출력 포맷
-void printMessage(WINDOW *window, const char *format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    vwprintw(window, format, args);
-    va_end(args);
-    wrefresh(window);
-}
-
-// 프로그램 종료시 수행
-void onClose()
-{
-    // ncurses윈도우 종료
-    delwin(MessageWindow);
-    delwin(MessageWindowBorder);
-    delwin(CommandWindow);
-    delwin(CommandWindowBorder);
-    delwin(InputWindow);
-    delwin(InputWindowBorder);
-    endwin();
 }
