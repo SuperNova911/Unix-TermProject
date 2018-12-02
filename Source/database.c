159b8d57a5df6b72fe6720427c914bccd04c28aa
Dopa99 <tychus77@naver.com>
2018-12-02 14:38:04 +0900

    데이터베이스 리워크

--- Source/database.c
+++ Source/database.c
@@ -1,87 +1,388 @@
 #include "database.h"
 
-bool createNewDatabase() { return true; }
-bool connectToDatabase() { return true; }
-bool closeDatabase() { return true; }
+MYSQL *Connection; 
 
-bool loadUser(User *userList, int amount, int lectureID)
+void printError()
 {
-	for (int index = 0; index < amount; index++)
+	printf("[ERROR] code: '%d', message: '%s'\n", mysql_errno(Connection), mysql_error(Connection));
+}
+
+bool executeQuery(char *query)
+{
+	if (mysql_query(Connection, query) != 0)
 	{
-		memset(&userList[index], 0, sizeof(User));
-		sprintf(userList[index].studentID, "%s%05d", "2015", index);
-		strncpy(userList[index].hashedPassword, "1234567890", sizeof(userList[index].hashedPassword));
-		sprintf(userList[index].userName, "%04d", index);
-		userList[index].role = (index % 3) + 1;
-		time(&userList[index].registerDate);
+		printError();
+		return false;
 	}
+
 	return true;
 }
-bool loadUserByID(User *user, char *studentID)
+
+bool initializeDatabase()
 {
-	memset(user, 0, sizeof(User));
-	strncpy(user->studentID, studentID, sizeof(user->studentID));
-	strncpy(user->hashedPassword, "thisispassword", sizeof(user->hashedPassword));
-	strncpy(user->userName, "김수환", sizeof(user->userName));
-	user->role = DB_Student;
-	time(&user->registerDate);
+	Connection = mysql_init(NULL);
+	if (Connection == NULL)
+	{
+		printf("Insufficient memory\n");
+		return false;
+	}
+
+	return true;
+}
+
+void createTable()
+{
+	executeQuery("CREATE TABLE IF NOT EXISTS `User` (`studentID` VARCHAR(16) NOT NULL, `hashedPassword` VARCHAR(64) NOT NULL, `userName` VARCHAR(16) NOT NULL, `role` INT NOT NULL, `registerDate` BIGINT NOT NULL, PRIMARY KEY (`studentID`))");
+	executeQuery("CREATE TABLE IF NOT EXISTS `Lecture` (`lectureID` INT NOT NULL, `lectureName` VARCHAR(64) NOT NULL, `professorID` VARCHAR(16) NOT NULL, `memberCount` INT NOT NULL, `createDate` BIGINT NOT NULL, PRIMARY KEY (`lectureID`))");
+	executeQuery("CREATE TABLE IF NOT EXISTS `LectureMember` (`lectureID` INT NOT NULL, `studentID` VARCHAR(16) NOT NULL)");
+	executeQuery("CREATE TABLE IF NOT EXISTS `AttendanceCheckLog` (`lectureID` INT NOT NULL, `studentID` VARCHAR(16) NOT NULL, `IP` VARCHAR(16) NOT NULL, `quizAnswer` VARCHAR(512) NOT NULL, `checkDate` BIGINT NOT NULL)");
+	executeQuery("CREATE TABLE IF NOT EXISTS `ChatLog` (`lectureID` INT NOT NULL, `userName` VARCHAR(16) NOT NULL, `message` VARCHAR(512) NOT NULL, `date` BIGINT NOT NULL)");
+}
+
+bool connectToDatabase()
+{
+	const char *host = "right.jbnu.ac.kr";
+	const char *user = "A201210927";
+	const char *password = "q1234";
+	const char *database = "A201210927";
+	unsigned int port = 0;
+	const char *socket = NULL;
+	unsigned long clientFlag = CLIENT_MULTI_STATEMENTS;
+
+	if (mysql_real_connect(Connection, host, user, password, database, port, socket, clientFlag) == NULL)
+	{
+		printError();
+		return false;
+	}
+
+	return true;
+}
+
+bool closeDatabase()
+{
+	if (Connection == NULL)
+	{
+		fprintf(stderr, "closeDatabase: Already closed\n");
+		return false;
+	}
+
+	mysql_close(Connection);
+	return true;
+}
+
+User loadUserByID(char *studentID, bool *dbResult)
+{
+	char query[512];
+	sprintf(query, "SELECT * FROM `User` WHERE studentID = '%s'", studentID);
+	executeQuery(query);
+
+	User user;
+	resetUser(&user);
+
+	MYSQL_RES *result;
+	result = mysql_store_result(Connection);
+	if (result == NULL)
+	{
+		printError();
+		*dbResult = false;
+		return user;
+	}
+
+	MYSQL_ROW row;
+	while ((row = mysql_fetch_row(result)) == NULL)
+	{
+		user = buildUser(row[0], row[1], row[2], atoi(row[3]), atol(row[4]));
+		mysql_free_result(result);
+
+		*dbResult = true;
+		return user;
+	}
+
+	*dbResult = false;
+	return user;
+}
+
+bool saveUser(User *user)
+{
+	char query[512];
+	sprintf(query, "REPLACE INTO `User` (`studentID`, `hashedPassword`, `userName`, `role`, `registerDate`) VALUES ('%s', '%s', '%s', '%d', '%ld')",
+		user->studentID, user->hashedPassword, user->userName, user->role, user->registerDate);
 	
-	return true;
+	return executeQuery(query);
 }
-bool registerUser(User *user) { return true; }
-bool removeUser(char *studentID) { return true; }
-bool loginUser(char *studentID, char *hashedPassword) { return true; }
-bool clearUser() { return true; }
 
-bool loadLecture(Lecture *lectureList, int amount)
+bool removeUser(char *studentID)
 {
-	for (int index = 0; index < amount; index++)
+	char query[512];
+	sprintf(query, "DELETE FROM `User` WHERE studentID = '%s'", studentID);
+
+	return executeQuery(query);
+}
+
+bool clearUser()
+{
+	return executeQuery("DELETE FROM `User`");
+}
+
+bool isUserMatch(char *studentID, char *hashedPassword)
+{
+	char query[512];
+	sprintf(query, "SELECT * FROM `User` WHERE studentID = '%s' AND hashedPassword = '%s'", studentID, hashedPassword);
+
+	MYSQL_RES *result;
+	result = mysql_store_result(Connection);
+	if (result == NULL)
 	{
-		memset(&lectureList[index], 0, sizeof(Lecture));
-		lectureList[index].lectureID = index + 1;
-		lectureList[index].memberCount = index;
-		strncpy(lectureList[index].professorID, "201510743", sizeof(lectureList[index].professorID));
-		strncpy(lectureList[index].lectureName, "201510743", sizeof(lectureList[index].lectureName));
-		for (int memberIndex = 0; memberIndex < index; memberIndex++)
-		{
-			strncpy(lectureList[index].memberList[memberIndex], "201510743", sizeof(lectureList[index].memberList[memberIndex]));
-		}
-		time(&lectureList[index].createDate);
+		printError();
+		return false;
+	}
+
+	MYSQL_ROW row;
+	while ((row = mysql_fetch_row(result)) == NULL)
+	{
+		return true;
+	}
+
+	return false;
+}
+
+Lecture loadLectureByName(char *lectureName, bool *dbResult)
+{
+	char query[512];
+	sprintf(query, "SELECT * FROM `Lecture` WHERE lectureName = '%s'", lectureName);
+	executeQuery(query);
+
+	Lecture lecture;
+	resetLecture(&lecture);
+
+	MYSQL_RES *result;
+	result = mysql_store_result(Connection);
+	if (result == NULL)
+	{
+		printError();
+		*dbResult = false;
+		return lecture;
+	}
+
+	MYSQL_ROW row;
+	while ((row = mysql_fetch_row(result)) == NULL)
+	{
+		buildLecture(&lecture, atoi(row[0]), row[1], row[2], atol(row[4]));
+		loadLectureMemberList(&lecture);
+
+		mysql_free_result(result);
+
+		*dbResult = true;
+		return lecture;
+	}
+
+	*dbResult = false;
+	return lecture;
+}
+
+int loadLectureList(Lecture *lectureList, int amount)
+{
+	char query[512];
+	sprintf(query, "SELECT * FROM `Lecture` LIMIT %d", amount);
+	executeQuery(query);
+
+	int index = 0;
+
+	MYSQL_RES *result;
+	result = mysql_store_result(Connection);
+	if (result == NULL)
+	{
+		printError();
+		return index;
+	}
+
+	MYSQL_ROW row;
+	while (((row = mysql_fetch_row(result)) == NULL) && index < amount)
+	{
+		buildLecture(&lectureList[index], atoi(row[0]), row[1], row[2], atol(row[4]));
+		loadLectureMemberList(&lectureList[index]);
+		index++;
+	}
+
+	mysql_free_result(result);
+	return index;
+}
+
+bool saveLecture(Lecture *lecture)
+{
+	char query[512];
+	sprintf(query, "REPLACE INTO `Lecture` (`lectureID`, `lectureName`, `professorID`, `memberCount`, `createDate`) VALUES ('%d', '%s', '%s', '%d', '%ld')",
+		lecture->lectureID, lecture->lectureName, lecture->professorID, lecture->memberCount, lecture->createDate);
+	
+	saveLectureMemberList(lecture);
+	
+	return executeQuery(query);
+}
+
+bool removeLectureByID(int lectureID)
+{
+	char query1[512], query2[512];
+	sprintf(query1, "DELETE FROM `Lecture` WHERE lectureID = '%d'", lectureID);
+	sprintf(query2, "DELETE FROM `LectureMember` WHERE lectureID = '%d'", lectureID);
+
+	return executeQuery(query1) && executeQuery(query2);
+}
+
+bool loadLectureMemberList(Lecture *lecture)
+{
+	char query[512];
+	sprintf(query, "SELECT * FROM `LectureMember` WHERE lectureID = '%d'", lecture->lectureID);
+	executeQuery(query);
+
+	MYSQL_RES *result;
+	result = mysql_store_result(Connection);
+	if (result == NULL)
+	{
+		printError();
+		return false;
+	}
+
+	MYSQL_ROW row;
+	for (int index = 0; ((row = mysql_fetch_row(result)) == NULL) && lecture->memberCount < MAX_LECTURE_MEMBER; index++)
+	{
+		strncpy(lecture->memberList[index], row[1], sizeof(lecture->memberList[index]));
+		lecture->memberCount++;
 	}
 
 	return true;
 }
-bool loadLectureByID(Lecture *lecture, int lectureID)
+
+void saveLectureMemberList(Lecture *lecture)
 {
-	memset(lecture, 0, sizeof(Lecture));
-	lecture->lectureID = 10;
-	lecture->memberCount = 2;
-	strncpy(lecture->professorID, "201510743", sizeof(lecture->professorID));
-	strncpy(lecture->lectureName, "201510743", sizeof(lecture->lectureName));
+	char query[512];
 	for (int index = 0; index < lecture->memberCount; index++)
-		strncpy(lecture->memberList[index], "201510743", sizeof(lecture->memberList[index]));
-	time(&lecture->createDate);
-	
-	return true;
-}
-bool createLecture(Lecture *lecture) { return true; }
-bool removeLecture(int lectureID) { return true; }
-bool lecture_registerUser(int lectureID, char *studentID) { return true; }
-bool lecture_deregisterUser(int lectureID, char *studentID) { return true; }
-bool clearLecture() { return true; }
-
-bool loadAttendanceCheckLog(AttendanceCheckLog *checkLogList, int amount, int lectureID)
-{
-	for (int index = 0; index < amount; index++)
 	{
-		memset(&checkLogList[index], 0, sizeof(AttendanceCheckLog));
+		sprintf(query, "REPLACE INTO `LectureMember` (`lectureID`, `studentID`) VALUES ('%d', '%s')",
+			lecture->lectureID, lecture->memberList[index]);
+		executeQuery(query);
 	}
-	return true;
 }
-bool saveAttendanceCheckLog(AttendanceCheckLog *checkLog) { return true; }
-bool clearAttendanceCheckLog() { return true; }
 
-bool loadChatLog(ChatLog *chatLog, int amount, int lectureID) { return true; }
-bool saveChatLog(ChatLog *chatLog) { return true; }
-bool clearChatLog() { return true; }
+bool clearLecture()
+{
+	return executeQuery("DELETE FROM `Lecture`");
+}
 
+bool clearLectureMember()
+{
+	return executeQuery("DELETE FROM `LectureMember`");
+}
+
+int loadAttendanceCheckLogList(AttendanceCheckLog *checkLogList, int amount, int lectureID, time_t date)
+{
+	const int SEARCH_RANGE = 12 * 60 * 60;		// 12시간
+
+	char query[512];
+	sprintf(query, "DELETE FROM `AttendanceCheckLog` WHERE lectureID = '%d' AND checkDate BETWEEN '%ld' AND '%ld' LIMIT %d", 
+		lectureID, date - SEARCH_RANGE, date + SEARCH_RANGE, amount);
+
+	executeQuery(query);
+
+	int index = 0;
+
+	MYSQL_RES *result;
+	result = mysql_store_result(Connection);
+	if (result == NULL)
+	{
+		printError();
+		return index;
+	}
+
+	MYSQL_ROW row;
+	while (((row = mysql_fetch_row(result)) == NULL) && index < amount)
+	{
+		checkLogList[index].lectureID = atoi(row[0]);
+		strncpy(checkLogList[index].studentID, row[1], sizeof(checkLogList[index].studentID));
+		strncpy(checkLogList[index].IP, row[2], sizeof(checkLogList[index].IP));
+		strncpy(checkLogList[index].quizAnswer, row[3], sizeof(checkLogList[index].quizAnswer));
+		checkLogList[index].checkDate = atol(row[4]);
+
+		index++;
+	}
+
+	mysql_free_result(result);
+	return index;
+}
+
+bool saveAttendanceCheckLog(AttendanceCheckLog *checkLog)
+{
+	char query[1024];
+	sprintf(query, "REPLACE INTO `AttendanceCheckLog` (`lectureID`, `studentID`, `IP`, `quizAnswer`, `checkDate`) VALUES ('%d', '%s', '%s', '%s', '%ld')",
+		checkLog->lectureID, checkLog->studentID, checkLog->IP, checkLog->quizAnswer, checkLog->checkDate);
+	
+	return executeQuery(query);
+}
+
+bool removeAttendanceCheckLog(int lectureID, time_t date)
+{
+	const int SEARCH_RANGE = 12 * 60 * 60;		// 12시간
+
+	char query[512];
+	sprintf(query, "DELETE FROM `AttendanceCheckLog` WHERE lectureID = '%d' AND checkDate BETWEEN '%ld' AND '%ld'", 
+		lectureID, date - SEARCH_RANGE, date + SEARCH_RANGE);
+
+	return executeQuery(query);
+}
+
+bool clearAttendanceCheckLog()
+{
+	return executeQuery("DELETE FROM `AttendanceCheckLog`");
+}
+
+int loadChatLogList(ChatLog *chatLogList, int amount, int lectureID)
+{
+	char query[512];
+	sprintf(query, "SELECT * FROM `ChatLog` WHERE lectureID = '%d' LIMIT %d", lectureID, amount);
+	executeQuery(query);
+
+	int index = 0;
+
+	MYSQL_RES *result;
+	result = mysql_store_result(Connection);
+	if (result == NULL)
+	{
+		printError();
+		return index;
+	}
+
+	MYSQL_ROW row;
+	while (((row = mysql_fetch_row(result)) == NULL) && index < amount)
+	{
+		chatLogList[index].lectureID = atoi(row[0]);
+		strncpy(chatLogList[index].userName, row[1], sizeof(chatLogList[index].userName));
+		strncpy(chatLogList[index].message, row[2], sizeof(chatLogList[index].message));
+		chatLogList[index].date = atol(row[3]);
+
+		index++;
+	}
+
+	mysql_free_result(result);
+	return index;
+}
+
+bool saveChatLog(ChatLog *chatLog)
+{
+	char query[1024];
+	sprintf(query, "REPLACE INTO `ChatLog` (`lectureID`, `userName`, `message`, `date`) VALUES ('%d', '%s', '%s', '%ld')",
+		chatLog->lectureID, chatLog->userName, chatLog->message, chatLog->date);
+	
+	return executeQuery(query);
+}
+
+bool removeChatLog(int lectureID)
+{
+	char query[512];
+	sprintf(query, "DELETE FROM `ChatLog` WHERE lectureID = '%d'", lectureID);
+
+	return executeQuery(query);
+}
+
+bool clearChatLog()
+{
+	return executeQuery("DELETE FROM `ChatLog`");
+}
--- Source/database.h
+++ Source/database.h
@@ -1,38 +1,14 @@
+#pragma once
+
+#include "user.h"
+#include "lecture.h"
+
 #include <time.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-#define DB_PATH "데이터베이스 경로"
-#define MAX_LECTURE_MEMBER 60
-
-// 사용자 권한
-typedef enum
-{
-    DB_None = 0, DB_Admin = 1, DB_Student = 2, DB_Professor = 3
-} Role;                                 
-
-// 사용자 정보 구조체
-typedef struct User_t
-{
-    char studentID[16];
-    char hashedPassword[64];
-    char userName[16];
-    Role role; 				    
-    time_t registerDate;		
-} User;
-
-// 강의 정보 구조체
-typedef struct Lecture_t
-{
-    int lectureID;
-    int memberCount;
-    char professorID[16];
-    char lectureName[128];
-    char memberList[MAX_LECTURE_MEMBER][16];
-    time_t createDate;
-} Lecture;
+#include <mysql.h>
 
 // 출석체크 기록 구조체
 typedef struct AttendanceCheckLog_t
@@ -53,31 +29,32 @@ typedef struct ChatLog_t
     time_t date;
 } ChatLog;
 
-bool createNewDatabase();       // 새로운 데이터베이스 생성
-//bool connectToDatabase();       // 데이터베이스에 연결
-bool closeDatabase();           // 데이터베이스 닫기
+bool initializeDatabase();
+void createTable();
+bool connectToDatabase();
+bool closeDatabase();
 
-bool loadUser(User *userList, int amount, int lectureID);   // DB에서 lectureID가 일치하는 사용자 구조체 배열 반환
-bool loadUserByID(User *user, char *studentID);             // DB에서 studentID가 일치하는 사용자 구조체 반환
-bool registerUser(User *user);                              // DB에 새로운 사용자 정보 저장
-bool removeUser(char *studentID);                           // DB에서 studentID가 일치하는 사용자 삭제
-bool loginUser(char *studentID, char *hashedPassword);      // DB에서 studentID와 hashedPassword가 일치하는 사용자가 있는지 확인
-bool clearUser();                                           // User 테이블 초기화
+User loadUserByID(char *studentID, bool *dbResult);
+bool saveUser(User *user);
+bool removeUser(char *studentID);
+bool clearUser();
+bool isUserMatch(char *studentID, char *hashedPassword);
 
-bool loadLecture(Lecture *lectureList, int amount);             // DB에서 강의 구조체 배열 반환
-bool loadLectureByID(Lecture *lecture, int lectureID);          // DB에서 lectureID가 일치하는 강의 정보 구조체 반환
-bool createLecture(Lecture *lecture);                           // DB에 새로운 강의 정보 저장
-bool removeLecture(int lectureID);                              // DB에서 lectureID가 일치하는 강의 삭제
-bool lecture_registerUser(int lectureID, char *studentID);      // DB에서 lectureID가 일치하는 강의의 memberList에 studentID추가
-bool lecture_deregisterUser(int lectureID, char *studentID);    // DB에서 lectureID가 일치하는 강의의 memberList에 studentID삭제
-bool clearLecture();                                            // Lecture 테이블 초기화
+Lecture loadLectureByName(char *lectureName, bool *dbResult);
+int loadLectureList(Lecture *lectureList, int amount);
+bool saveLecture(Lecture *lecture);
+bool removeLectureByID(int lectureID);
+bool loadLectureMemberList(Lecture *lecture);
+void saveLectureMemberList(Lecture *lecture);
+bool clearLecture();
+bool clearLectureMember();
 
-bool loadAttendanceCheckLog(AttendanceCheckLog *checkLogList, int amount, int lectureID);   // DB에서 lectureID가 일치하는 출석체크 기록 구조체 배열 반환
-bool saveAttendanceCheckLog(AttendanceCheckLog *checkLog);       // DB에 출석체크 기록 저장
-bool clearAttendanceCheckLog();                                 // AttendanceCheckLog 테이블 초기화
+int loadAttendanceCheckLogList(AttendanceCheckLog *checkLogList, int amount, int lectureID, time_t date);
+bool saveAttendanceCheckLog(AttendanceCheckLog *checkLog);
+bool removeAttendanceCheckLog(int lectureID, time_t date);
+bool clearAttendanceCheckLog();
 
-bool loadChatLog(ChatLog *chatLog, int amount, int lectureID);       // DB에서 lectureID가 일치하는 채팅 기록 구조체 배열 반환
-bool saveChatLog(ChatLog *chatLog);                                       // DB에 채팅 기록 저장
+int loadChatLogList(ChatLog *chatLogList, int amount, int lectureID);
+bool saveChatLog(ChatLog *chatLog);
+bool removeChatLog(int lectureID);
 bool clearChatLog();
-
-
